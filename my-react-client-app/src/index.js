fbf9eeec2009a9036ba32fd8114bda5a69a56270
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-02-28 22:15:26 -0500

    hashrouter

--- my-react-client-app/src/index.js
+++ my-react-client-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
@@ -9,11 +9,11 @@ import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
