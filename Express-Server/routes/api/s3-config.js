9e221ffbd24219eb98282043e95caf1b59ea9334
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-03-13 15:38:19 -0400

    public folder

---
+++ Express-Server/routes/api/s3-config.js
@@ -0,0 +1,37 @@
+// {
+//     "Version": "2012-10-17",
+//     "Id": "Policy1710342070560",
+//     "Statement": [
+//         {
+//             "Sid": "Stmt1710342057551",
+//             "Effect": "Allow",
+//             "Principal": "*",
+//             "Action": [
+//                 "s3:DeleteObject",
+//                 "s3:GetObject"
+//             ],
+//             "Resource": "arn:aws:s3:::s3-image-upload-bucket/*"
+//         }
+//     ]
+// }
+
+// [
+//     {
+//         "AllowedHeaders": [
+//             "*"
+//         ],
+//         "AllowedMethods": [
+//             "GET",
+//             "HEAD",
+//             "PUT",
+//             "DELETE"
+//         ],
+//         "AllowedOrigins": [
+//             "*"
+//         ],
+//         "ExposeHeaders": []
+//     }
+// ]
+
+
+// {"AWS_ACCESS_KEY_ID":"AKIATQRG6BLGZ5MAXYPB","AWS_SECRET_ACCESS_KEY":"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","MONGOOSE_DB_CONNECTION_STRING":"mongodb+srv://kumardvplr:4UQw7tYxwHVnDRJV@inventory.22v2zgv.mongodb.net/?retryWrites=true&w=majority&appName=Inventory","API_KEY":"eaed3af1eemshe894b69298432ccp10d934jsn9fa0b2a61780\"","API_HOST":"books-api7.p.rapidapi.com"}
