689f1e0fe7266901c8f77e0b99aa58de099658c6
Prashanth Deekonda <prashanth.deekonda@gmail.com>
2024-03-31 11:10:01 -0400

    login and signup page update

--- frontend/src/routes/login/login.component.jsx
+++ frontend/src/routes/login/login.component.jsx
@@ -1,5 +1,84 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+// React Notification
+import { NotificationManager } from "react-notifications";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const signIn = (e) => {
+    if (!username) {
+      NotificationManager.error("Username shouldn't be empty", "Error !");
+      return;
+    } else if (!password) {
+      NotificationManager.error("Password shouldn't be empty", "Error !");
+      return;
+    }
+    e.preventDefault();
+    console.log('signIn', {username, password})
+    // const postURL = `${baseURL}api/inventory`;
+  };
+
+  return (
+    <div>
+      <div
+        class="container mt-5"
+        style={{
+          width: "42rem",
+          padding: "2rem",
+          border: "2px solid lightgray",
+          borderRadius: "5px",
+        }}
+      >
+        <form noValidate>
+          {/* <h1> Welcome to my React Application</h1>
+          <span>You can view my profile, addition, books and inventory </span> */}
+          <h3 style={{ textAlign: "center" }}>Sign In</h3>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="username"
+              className="form-control"
+              placeholder="Enter username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          {/* <div className="mb-3">
+            <div className="custom-control custom-checkbox">
+              <input
+                type="checkbox"
+                className="custom-control-input"
+                id="customCheck1"
+              />
+              <label className="custom-control-label" htmlFor="customCheck1">
+                Remember me
+              </label>
+            </div>
+          </div> */}
+          <div className="d-grid">
+            <button type="button" className="btn btn-primary" onClick={signIn}>
+              Submit
+            </button>
+          </div>
+          <p className="mt-3">
+            New Here&nbsp;
+            <Link to="/signup">Please SignUp?</Link>
+            {/* <a href="/signup"></a> */}
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/routes/signup/signup.component.jsx
+++ frontend/src/routes/signup/signup.component.jsx
@@ -1,5 +1,94 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// React Notification
+import { NotificationManager } from "react-notifications";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+
+  const registerUser = (e) => {
+    if (!username) {
+      NotificationManager.error("Username shouldn't be empty", "Error !");
+      return;
+    } else if (!email) {
+      NotificationManager.error("Email shouldn't be empty", "Error !");
+      return;
+    } else if (!password) {
+      NotificationManager.error("Password shouldn't be empty", "Error !");
+      return;
+    }
+    e.preventDefault();
+    // const postURL = `${baseURL}api/inventory`;
+    console.log("signUp", { username, password, email });
+  };
+
+  return (
+    <div>
+      <div
+        class="container mt-5"
+        style={{
+          width: "42rem",
+          padding: "2rem",
+          border: "2px solid lightgray",
+          borderRadius: "5px",
+        }}
+      >
+        <form noValidate>
+          <h3 style={{ textAlign: "center" }}>Sign Up</h3>
+          <div className="mb-3">
+            <label>Username</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          {/* <div className="mb-3">
+            <label>Last name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Last name"
+            />
+          </div> */}
+          <div className="mb-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={registerUser}
+            >
+              Sign Up
+            </button>
+          </div>
+          <p className="mt-2">
+            Registered Already &nbsp;
+            <Link to="/login">sign in?</Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
